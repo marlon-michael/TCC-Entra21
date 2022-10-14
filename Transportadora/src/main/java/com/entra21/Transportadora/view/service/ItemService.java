92a1f6c89d9ee861ee13eb500a7223b6b9b2a9b0
Marlon Michael Pereira <marlonp@furb.br>
2022-10-13 21:39:42 -0300

    Renaming variables

--- Transportadora/src/main/java/com/entra21/Transportadora/view/service/ItemService.java
+++ Transportadora/src/main/java/com/entra21/Transportadora/view/service/ItemService.java
@@ -26,40 +26,39 @@ public class ItemService {
     private PessoaRepository pessoaRepository;
 
     public List<ItemDTO> getAllItem() {
-        return itemRepository.findAll().stream().map(fr -> {
-            ItemDTO dto = new ItemDTO();
-            dto.setLocalizador(fr.getLocalizador());
-            dto.setLocalEntrega(fr.getLocalEntrega());
-            dto.setNomeRecebedor(fr.getNomeRecebedor());
-            dto.setStatus(fr.getStatus());
-            if (fr.getPessoa() == null){
-                return dto;
+        return itemRepository.findAll().stream().map(itemEntity -> {
+            ItemDTO itemDTO = new ItemDTO();
+            PessoaDTO pessoaDTO = new PessoaDTO();
+
+            if (itemEntity.getPessoa() != null){
+                pessoaDTO.setNome(itemEntity.getPessoa().getNome());
+                pessoaDTO.setSobrenome(itemEntity.getPessoa().getSobrenome());
+                pessoaDTO.setTelefone(itemEntity.getPessoa().getTelefone());
+                pessoaDTO.setCpf(itemEntity.getPessoa().getCpf());
             }
-                PessoaDTO pessoaDTO = new PessoaDTO();
-                pessoaDTO.setNome(fr.getPessoa().getNome());
-                pessoaDTO.setSobrenome(fr.getPessoa().getSobrenome());
-                pessoaDTO.setTelefone(fr.getPessoa().getTelefone());
-                pessoaDTO.setCpf(fr.getPessoa().getCpf());
-                dto.setPessoaItem(pessoaDTO);
-                return dto;
 
+            itemDTO.setPessoaItem(pessoaDTO);
+            itemDTO.setLocalizador(itemEntity.getLocalizador());
+            itemDTO.setLocalEntrega(itemEntity.getLocalEntrega());
+            itemDTO.setNomeRecebedor(itemEntity.getNomeRecebedor());
+            itemDTO.setStatus(itemEntity.getStatus());
 
+            return itemDTO;
         }).collect(Collectors.toList());
     }
 
-
-
     public void saveItem(ItemAddDTO input) {
-        ItemEntity newEntity = new ItemEntity();
-        newEntity.setLocalizador(input.getLocalizador());
-        newEntity.setLocalEntrega(input.getLocalEntrega());
-        newEntity.setNomeRecebedor(input.getNomeRecebedor());
-        newEntity.setStatus(input.getStatus());
+        ItemEntity itemEntity = new ItemEntity();
         PessoaEntity pessoa = new PessoaEntity();
-        pessoa.setIdPessoa(input.getPessoaItem().getIdPessoa());
-        newEntity.setPessoa(pessoa);
 
-        itemRepository.save(newEntity);
+        pessoa.setIdPessoa(input.getPessoaItem().getIdPessoa());
+        itemEntity.setPessoa(pessoa);
+        itemEntity.setLocalizador(input.getLocalizador());
+        itemEntity.setLocalEntrega(input.getLocalEntrega());
+        itemEntity.setNomeRecebedor(input.getNomeRecebedor());
+        itemEntity.setStatus(input.getStatus());
+
+        itemRepository.save(itemEntity);
     }
 
     public void deleteItem(Long id) {
@@ -67,31 +66,33 @@ public class ItemService {
     }
 
     public ItemUpDTO updateStatusItem(Long id, String novoStatus) {
-        ItemEntity e = itemRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item não encontrado!"));
-        e.setStatus(novoStatus);
-        e = itemRepository.save(e);
-        ItemUpDTO dto = new ItemUpDTO();
-        dto.setStatus(e.getStatus());
-        return dto;
+        ItemEntity itemEntity = itemRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item não encontrado!"));
+        ItemUpDTO itemUpDTO = new ItemUpDTO();
+
+        itemUpDTO.setStatus(novoStatus);
+
+        itemEntity.setStatus(novoStatus);
+        itemRepository.save(itemEntity);
+
+        return itemUpDTO;
     }
 
     public ItemUpDTO itemUpDTO(Long id, ItemUpDTO itemDTO) {
-        ItemEntity e = itemRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item não encontrado!"));
-        e.setStatus(itemDTO.getStatus());
-        e.setNomeRecebedor(itemDTO.getNomeRecebedor());
-        e.setLocalizador(itemDTO.getLocalizador());
-        e.setLocalEntrega(itemDTO.getLocalEntrega());
+        ItemEntity itemEntity = itemRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item não encontrado!"));
+        itemEntity.setStatus(itemDTO.getStatus());
+        itemEntity.setNomeRecebedor(itemDTO.getNomeRecebedor());
+        itemEntity.setLocalizador(itemDTO.getLocalizador());
+        itemEntity.setLocalEntrega(itemDTO.getLocalEntrega());
 
         PessoaEntity pessoaDTO = new PessoaEntity();
-        pessoaDTO.setIdPessoa(e.getPessoa().getIdPessoa());
-        e.setPessoa(pessoaDTO);
-        itemDTO.setIdItem(e.getIdItem());
-        e.setPessoa(pessoaRepository.findById(itemDTO.getPessoaItem().getIdPessoa()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST)));
-        e = itemRepository.save(e);
-        itemDTO.setIdItem(e.getIdItem());
+        pessoaDTO.setIdPessoa(itemEntity.getPessoa().getIdPessoa());
+        itemEntity.setPessoa(pessoaDTO);
+        itemDTO.setIdItem(itemEntity.getIdItem());
+        itemEntity.setPessoa(pessoaRepository.findById(itemDTO.getPessoaItem().getIdPessoa()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST)));
+        itemEntity = itemRepository.save(itemEntity);
+        itemDTO.setIdItem(itemEntity.getIdItem());
 
         return itemDTO;
     }
-
 
 }
