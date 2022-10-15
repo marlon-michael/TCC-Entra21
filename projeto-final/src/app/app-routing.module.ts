7c8dea6200d87024b8df36f239a3f881f13381fd
Gabriela Schirmer <gschirmer@furb.br>
2022-10-14 22:00:57 -0300

    Sextou

--- projeto-final/src/app/app-routing.module.ts
+++ projeto-final/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConfiguracoesComponent } from './configuracoes/configuracoes.component';
+import { LocalizadorComponent } from './localizador/localizador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: 'localizador', component: LocalizadorComponent},
+ { path: 'config', component: ConfiguracoesComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagInicialLoginComponent } from './pag-inicial-login/pag-inicial-login.component';
+import { LocalizadorComponent } from './localizador/localizador.component';
+import { ConfiguracoesComponent } from './configuracoes/configuracoes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagInicialLoginComponent,
+    LocalizadorComponent,
+    ConfiguracoesComponent
   ],
   imports: [
     BrowserModule,
