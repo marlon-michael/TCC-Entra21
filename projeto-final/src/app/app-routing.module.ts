beb935801cb3357a4f64d79d7ed818bbe2fb42bd
Marlon Michael Pereira <marlonp@furb.br>
2022-10-17 21:44:36 -0300

    Integrating Frontend to Backend

--- projeto-final/src/app/app-routing.module.ts
+++ projeto-final/src/app/app-routing.module.ts
@@ -1,27 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HttpClientModule }from '@angular/common/http';
 import { AboutComponent } from 'src/pages/about/about.component';
 import { ContactComponent } from 'src/pages/contact/contact.component';
 import { HomeComponent } from 'src/pages/home/home.component';
 import { LoginComponent } from 'src/pages/login/login.component';
-import { ConfiguracoesComponent } from './logado/configuracoes/configuracoes.component';
-import { LocalizadorComponent } from './logado/localizador/localizador.component';
-import { PagInicialLoginComponent } from './logado/pag-inicial-login/pag-inicial-login.component'; 
+import { ConfiguracoesComponent } from 'src/app/logado/configuracoes/configuracoes.component';
+import { LocalizadorComponent } from 'src/app/logado/localizador/localizador.component';
+import { PagInicialLoginComponent } from 'src/app/logado/pag-inicial-login/pag-inicial-login.component'; 
 
 const routes: Routes = [
 {path: '', component:HomeComponent},
 {path:'login',component:LoginComponent},
 {path: 'about',component:AboutComponent},
 {path: 'contact',component:ContactComponent},
-{ path: 'localizador', component: LocalizadorComponent},
-{ path: 'config', component: ConfiguracoesComponent},
-{path: 'app-pag-inicial-login', component: PagInicialLoginComponent}
+{path: 'local', component:LocalizadorComponent},
+{path: 'config', component:ConfiguracoesComponent},
+{path: 'app-pag-inicial-login', component:PagInicialLoginComponent}
 ];
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
