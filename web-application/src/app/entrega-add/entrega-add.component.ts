e219496ae1ed71b8bd0d5872a5b3ad742b8ed76e
unknown <marlonmich7@gmail.com>
2022-12-04 15:23:34 -0300

    added error messages in delivery page

--- web-application/src/app/entrega-add/entrega-add.component.ts
+++ web-application/src/app/entrega-add/entrega-add.component.ts
@@ -56,11 +56,10 @@ export class EntregaAddComponent {
       entregador: {cpf: this.entregaForm.get("motoristaCPF")?.value},
       entregaTrecho: entregaTrechos,
       itens: itens
-    }).subscribe(res => {
-      this.error = ""
-    }, error => {
-      this.error = "Os dados informados podem estar incorretos ou não estarem presentes em nossos bancos. Verifique e tente novamente";
-    })
+    }).subscribe(
+      res => this.error = "", 
+      error => this.error = "Os dados informados podem estar incorretos ou não estarem presentes em nossos bancos. Verifique e tente novamente"
+    )
   };
 }
 
